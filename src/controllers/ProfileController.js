0c451766f3be9b4c23219ca5916d9c636112a59b
masud1989 <mranasrj@gmail.com>
2022-06-27 08:10:24 +0600

    get-login-token

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -1,4 +1,5 @@
 const ProfileModel = require("../models/ProfileModel");
+const jwt = require('jsonwebtoken');
 
 exports.CreateProfile = (req,res) => {
     const reqBody = req.body;
@@ -17,6 +18,20 @@ exports.UserLogin = (req,res) => {
     const Password = req.body['Password'];
     // res.status(200).json({status:"Success", data:UserName})
     // res.status(200).json({status:"Success", data:Password})
-    
+    ProfileModel.find({UserName:UserName, Password:Password}, (error,data)=>{
+        if(error){
+            res.status(400).json({status:"Failed", data:error})
+        }
+        else{
+            if( data.length>0 ){
+                const payLoad = {exp:Math.floor(Date.now()/1000)+(24*60*60), data:data[0]}
+                const token = jwt.sign(payLoad, "SecretKey");
+                res.status(200).json({status:"Success", token:token, data:data})
+            }
+            else{
+                res.status(401).json({status:"Unauthorized"})
+            }
+        }
+    })
     
 }
