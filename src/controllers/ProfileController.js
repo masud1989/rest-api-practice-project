3a7dda9b2ba23fe217be526b6ccf708c1b8f8c2f
masud1989 <mranasrj@gmail.com>
2022-06-28 17:01:41 +0600

    update-profile

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -48,3 +48,17 @@ exports.SelectProfile = (req,res) => {
         }
     })
 }
+
+exports.UpdateProfile = (req,res) => {
+    const UserName = req.headers["username"];
+    const reqBody = req.body;
+
+    ProfileModel.updateOne({UserName:UserName}, {$set:reqBody}, {upsert:true}, (error,data)=>{
+        if(error){
+            res.status(400).json({status:"Failed", data:error})
+        }
+        else{
+            res.status(200).json({status:"Success", data:data})
+        }
+    })
+}
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const {CreateProfile, UserLogin, SelectProfile} = require('../controllers/ProfileController');
+const {CreateProfile, UserLogin, SelectProfile, UpdateProfile} = require('../controllers/ProfileController');
 const AuthVerifyMiddleware = require('../middleware/AuthVerifyMiddleware');
 
 
 
 router.post('/CreateProfile', CreateProfile);
 router.post('/UserLogin', UserLogin);
+
+// with jwt Token 
 router.get('/SelectProfile', AuthVerifyMiddleware, SelectProfile);
+router.post('/UpdateProfile', AuthVerifyMiddleware, UpdateProfile);
 
 
 module.exports = router;
